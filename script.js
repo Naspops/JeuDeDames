af4fdc6c995c26183c1509480027cd9cd78fad69
neirdAxD <amahe69@gmail.com>
2017-03-30 10:25:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,15 @@ function placement(){
 			}
 		
 }
+
+var t1 = 60;
+$(".compteur").append("<p>"+t1+"</p>");
+
+function decr(){
+	t1 = t1-1;
+	$(".compteur").html("<p>"+t1+"</p>");
+}
+
+$(".compteur").on("click", function(){
+	setInterval(decr, 1000);
+});
