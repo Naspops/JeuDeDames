99168eea9cc31df108fe1e45d3c24a868d3e6bb3
Marciouf <marco200197@gmail.com>
2017-04-05 09:50:04 +0200

    Petits changements

--- script.js
+++ script.js
@@ -26,15 +26,15 @@ document.getElementById('element').innerHTML = tab;
 function placement(){
 	for(var j=0; j<10; j++){
 		if(j%2==0){
-			document.getElementById('cell-c'+j+'-L0').innerHTML += "<img style=width:40px;height:40px; src='image/PION_N' id=\"img-c"+j+"-L0\">";
-			document.getElementById('cell-c'+j+'-L2').innerHTML += "<img style=width:40px;height:40px; src='image/PION_N' id=\"img-c"+j+"-L2\">";
-			document.getElementById('cell-c'+j+'-L9').innerHTML += "<img style=width:40px;height:40px; src='image/PION_B' id=\"img-c"+j+"-L9\">";
-			document.getElementById('cell-c'+j+'-L7').innerHTML += "<img style=width:40px;height:40px; src='image/PION_B' id=\"img-c"+j+"-L7\">";
+			document.getElementById('cell-c'+j+'-L0').innerHTML += "<img style=width:40px;height:40px; src='image/PION_N.png' id=\"img-c"+j+"-L0\">";
+			document.getElementById('cell-c'+j+'-L2').innerHTML += "<img style=width:40px;height:40px; src='image/PION_N.png' id=\"img-c"+j+"-L2\">";
+			document.getElementById('cell-c'+j+'-L9').innerHTML += "<img style=width:40px;height:40px; src='image/PION_B.png' id=\"img-c"+j+"-L9\">";
+			document.getElementById('cell-c'+j+'-L7').innerHTML += "<img style=width:40px;height:40px; src='image/PION_B.png' id=\"img-c"+j+"-L7\">";
 		}else{
-			document.getElementById('cell-c'+j+'-L1').innerHTML += "<img style=width:40px;height:40px; src='image/PION_N' id=\"img-c"+j+"-L1\">";
-			document.getElementById('cell-c'+j+'-L3').innerHTML += "<img style=width:40px;height:40px; src='image/PION_N' id=\"img-c"+j+"-L3\">";
-			document.getElementById('cell-c'+j+'-L8').innerHTML += "<img style=width:40px;height:40px; src='image/PION_B' id=\"img-c"+j+"-L8\">";
-			document.getElementById('cell-c'+j+'-L6').innerHTML += "<img style=width:40px;height:40px; src='image/PION_B' id=\"img-c"+j+"-L6\">";
+			document.getElementById('cell-c'+j+'-L1').innerHTML += "<img style=width:40px;height:40px; src='image/PION_N.png' id=\"img-c"+j+"-L1\">";
+			document.getElementById('cell-c'+j+'-L3').innerHTML += "<img style=width:40px;height:40px; src='image/PION_N.png' id=\"img-c"+j+"-L3\">";
+			document.getElementById('cell-c'+j+'-L8').innerHTML += "<img style=width:40px;height:40px; src='image/PION_B.png' id=\"img-c"+j+"-L8\">";
+			document.getElementById('cell-c'+j+'-L6').innerHTML += "<img style=width:40px;height:40px; src='image/PION_B.png' id=\"img-c"+j+"-L6\">";
 		}
 	}		
 
@@ -54,8 +54,19 @@ function placement(){
 
 
 function jouer(){
-	for var(j)
+
 }
+
+
+function deplacement(){
+
+}
+
+
+
+
+
+
 
 var t1 = 60;
 $(".compteur").append("<p>"+t1+"</p>");
